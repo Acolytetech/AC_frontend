8b483b7a6bd27a9dc08e26cdd291d4763156794e
Acolytetech <slawaniya03@gmail.com>
2025-09-08 15:31:12 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+images: {
+    domains: ["picsum.photos"], // yahan remote domain add karo
+  },};
+
+export default nextConfig;
